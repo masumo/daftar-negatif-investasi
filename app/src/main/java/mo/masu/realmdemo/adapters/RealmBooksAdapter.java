195f97618c13fda79e7b2c234e156e99dcfb67f9
masumo <mas.kosumo@gmail.com>
2016-05-30 16:55:22 +0700

    first commit

---
+++ app/src/main/java/mo/masu/realmdemo/adapters/RealmBooksAdapter.java
@@ -0,0 +1,14 @@
+package mo.masu.realmdemo.adapters;
+
+import android.content.Context;
+
+import mo.masu.realmdemo.model.Book;
+import io.realm.RealmResults;
+
+public class RealmBooksAdapter extends RealmModelAdapter<Book> {
+
+    public RealmBooksAdapter(Context context, RealmResults<Book> realmResults, boolean automaticUpdate) {
+
+        super(context, realmResults, automaticUpdate);
+    }
+}
