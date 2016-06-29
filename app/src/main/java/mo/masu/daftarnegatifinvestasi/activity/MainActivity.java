dcfbd47e47d43d847c42bfd564d95381e80d9fc6
masumo <mas.kosumo@gmail.com>
2016-06-29 16:58:56 +0700

    initial start after project refactoring

---
+++ app/src/main/java/mo/masu/daftarnegatifinvestasi/activity/MainActivity.java
@@ -0,0 +1,139 @@
+package mo.masu.daftarnegatifinvestasi.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+
+//import app.androidhive.info.realm.app.Prefs;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import au.com.bytecode.opencsv.CSVReader;
+import mo.masu.daftarnegatifinvestasi.R;
+import mo.masu.daftarnegatifinvestasi.adapters.BooksAdapter;
+import mo.masu.daftarnegatifinvestasi.adapters.RealmBooksAdapter;
+import mo.masu.daftarnegatifinvestasi.model.Business;
+import mo.masu.daftarnegatifinvestasi.realm.RealmController;
+import io.realm.Realm;
+import io.realm.RealmResults;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BooksAdapter adapter;
+    private Realm realm;
+    private LayoutInflater inflater;
+    private FloatingActionButton fab;
+    private RecyclerView recycler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recycler = (RecyclerView) findViewById(R.id.recycler);
+
+        //get realm instance
+        this.realm = RealmController.with(this).getRealm();
+
+        //set toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        setupRecycler();
+
+        setRealmData();
+
+        /*if (!Prefs.with(this).getPreLoad()) {
+            setRealmData();
+        }
+        */
+
+        // refresh the realm instance
+        //RealmController.with(this).refresh();
+        // get all persisted objects
+        // create the helper adapter and notify data set changes
+        // changes will be reflected automatically
+        setRealmAdapter(RealmController.with(this).getBooks());
+
+        //Toast.makeText(this, "Press card item for edit, long press to remove item", Toast.LENGTH_LONG).show();
+    }
+
+    public void setRealmAdapter(RealmResults<Business> books) {
+
+        //RealmBooksAdapter realmAdapter = new RealmBooksAdapter(this.getApplicationContext(), books, true);
+        RealmBooksAdapter realmAdapter = new RealmBooksAdapter(this.getApplicationContext(), books);
+        // Set the data and tell the RecyclerView to draw
+        adapter.setRealmAdapter(realmAdapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    private void setupRecycler() {
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recycler.setHasFixedSize(true);
+
+        // use a linear layout manager since the cards are vertically scrollable
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recycler.setLayoutManager(layoutManager);
+
+        // create an empty adapter and add it to the recycler view
+        adapter = new BooksAdapter(this);
+        recycler.setAdapter(adapter);
+    }
+
+    // load data from csv file and store it into realm database
+    private void setRealmData() {
+
+        String next[] = {};
+        List<String[]> csvlist = new ArrayList<String[]>();
+        try {
+            InputStreamReader csvStreamReader = new InputStreamReader(
+                    MainActivity.this.getAssets().open(
+                            "data.csv"));
+
+            CSVReader reader = new CSVReader(csvStreamReader);
+            csvlist = reader.readAll();
+
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        ArrayList<Business> books = new ArrayList<>();
+        int i =0;
+        for(String[] data : csvlist)
+        {
+            Business book = new Business();
+            book.setId((int) (i+1 + System.currentTimeMillis()));
+            String kbli = data[0];
+            book.setKbli(kbli);
+            book.setName(data[2]);
+            String prosen_saham = data[3];
+            book.setForeignStock(prosen_saham);
+            book.setOtherReqs(data[4]);
+            book.setSector(data[5]);
+            books.add(book);
+            i++;
+        }
+
+        for (Business b : books) {
+            // Persist your data easily
+            realm.beginTransaction();
+            realm.copyToRealm(b);
+            realm.commitTransaction();
+        }
+
+        //Prefs.with(this).setPreLoad(true);
+
+    }
+}
---
+++ app/src/main/java/mo/masu/daftarnegatifinvestasi/adapters/BooksAdapter.java
@@ -0,0 +1,136 @@
+package mo.masu.daftarnegatifinvestasi.adapters;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import mo.masu.daftarnegatifinvestasi.R;
+//import mo.masu.realmdemo.app.Prefs;
+import mo.masu.daftarnegatifinvestasi.model.Business;
+import mo.masu.daftarnegatifinvestasi.realm.RealmController;
+import io.realm.Realm;
+
+public class BooksAdapter extends RealmRecyclerViewAdapter<Business> {
+
+    final Context context;
+    private Realm realm;
+    private LayoutInflater inflater;
+
+    public BooksAdapter(Context context) {
+
+        this.context = context;
+    }
+
+    // create new views (invoked by the layout manager)
+    @Override
+    public CardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // inflate a new card view
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.business_list, parent, false);
+        return new CardViewHolder(view);
+    }
+
+    // replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, final int position) {
+
+        realm = RealmController.getInstance().getRealm();
+
+        // get the article
+        final Business book = getItem(position);
+        // cast the generic view holder to our specific one
+        final CardViewHolder holder = (CardViewHolder) viewHolder;
+
+        // set value of each item on the business list
+        holder.textName.setText(book.getName());
+        holder.textKBLI.setText("KBLI: "+book.getKbli());
+        holder.textOtherReqs.setText(book.getOtherReqs());
+        String foreignStockValue = book.getForeignStock()+"%"+System.getProperty("line.separator")+"ASING";
+        holder.textForeignStock.setText(foreignStockValue);
+
+        // load the background image
+/*        if (book.getForeignStock() != null) {
+            Glide.with(context)
+                    .load(book.getForeignStock().replace("https", "http"))
+                    .asBitmap()
+                    .fitCenter()
+                    .into(holder.imageBackground);
+        }
+*/
+
+        // click action on each business item
+        holder.card.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View content = inflater.inflate(R.layout.business_detail, null);
+                final TextView detailName = (TextView) content.findViewById(R.id.name);
+                final TextView detailSector = (TextView) content.findViewById(R.id.sector);
+                final TextView detailKBLI = (TextView) content.findViewById(R.id.kbli);
+                final TextView detailReqs = (TextView) content.findViewById(R.id.req);
+                final TextView detailStock = (TextView) content.findViewById(R.id.foreign_stock);
+
+                detailName.setText(book.getName());
+                detailSector.setText(book.getSector());
+                detailKBLI.setText(book.getKbli());
+                detailReqs.setText(book.getOtherReqs());
+                detailStock.setText(book.getForeignStock());
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setView(content)
+                        .setTitle("Informasi Detail")
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+    }
+
+    // return the size of your data set (invoked by the layout manager)
+    public int getItemCount() {
+
+        if (getRealmAdapter() != null) {
+            return getRealmAdapter().getCount();
+        }
+        return 0;
+    }
+
+    public static class CardViewHolder extends RecyclerView.ViewHolder {
+
+        public CardView card;
+        public TextView textName;
+        public TextView textKBLI;
+        public TextView textOtherReqs;
+        //public ImageView imageBackground;
+        //public TextView textForeignStock;
+        public Button textForeignStock;
+
+        public CardViewHolder(View itemView) {
+            // standard view holder pattern with Butterknife view injection
+            super(itemView);
+
+            card = (CardView) itemView.findViewById(R.id.card_books);
+            textName = (TextView) itemView.findViewById(R.id.text_business_name);
+            textKBLI = (TextView) itemView.findViewById(R.id.text_kbli);
+            textOtherReqs = (TextView) itemView.findViewById(R.id.text_other_reqs);
+            //imageBackground = (ImageView) itemView.findViewById(R.id.image_background);
+            //textForeignStock = (TextView) itemView.findViewById(R.id.text_foreign_stock);
+            textForeignStock = (Button) itemView.findViewById(R.id.text_foreign_stock);
+            //  http://www.viralandroid.com/2016/01/circular-button-with-icon-and-text-in-android.html
+
+        }
+    }
+}
